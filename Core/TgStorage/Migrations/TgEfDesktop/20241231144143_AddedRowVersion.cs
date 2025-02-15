2c4a820e1c6eb93974ec86968bf97bcc108d240f
Damian Viktorovich Morozov <damian@morozov33.ru>
2025-02-15 17:10:43 +0300

    Added TgEfDesktopContext for TgStorage

---
+++ Core/TgStorage/Migrations/TgEfDesktop/20241231144143_AddedRowVersion.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+
+namespace TgStorage.Migrations.TgEfDesktop
+{
+    /// <inheritdoc />
+    public partial class AddedRowVersion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Core/TgStorage/Migrations/TgEfDesktop/20250112164624_UpdatedApps_AddedBotToken.cs
@@ -0,0 +1,39 @@
+﻿#nullable disable
+
+namespace TgStorage.Migrations.TgEfDesktop
+{
+    /// <inheritdoc />
+    public partial class UpdatedApps_AddedBotToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BOT_TOKEN",
+                table: "APPS",
+                type: "NVARCHAR(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IS_BOT",
+                table: "APPS",
+                type: "BIT",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BOT_TOKEN",
+                table: "APPS");
+
+            migrationBuilder.DropColumn(
+                name: "IS_BOT",
+                table: "APPS");
+        }
+    }
+}

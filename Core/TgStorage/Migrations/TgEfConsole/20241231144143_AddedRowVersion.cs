01977636fa27eafd0b901d7b5f04326d7a75e82c
Damian Viktorovich Morozov <damian@morozov33.ru>
2025-02-15 17:10:25 +0300

    Added TgEfConsoleContext for TgStorage

---
+++ Core/TgStorage/Migrations/TgEfConsole/20241231144143_AddedRowVersion.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+
+namespace TgStorage.Migrations.TgEfConsole
+{
+    /// <inheritdoc />
+    public partial class AddedRowVersion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

b70dd96c4663b05405baa6edf49f2b4494fb7567
Damian Viktorovich Morozov <damian@morozov33.ru>
2025-06-19 15:52:53 +0300

    Update tests

--- Tests/TgInfrastructureTest/Helpers/TgStringUtilsTests.cs
+++ Tests/TgInfrastructureTest/Helpers/TgStringUtilsTests.cs
@@ -17,31 +17,75 @@ public class TgStringUtilsTests
         return TgStringUtils.NormilizeTgName(input, isAddAt);
     }
 
-    [Test]
-    public void NormilizeTgNames_SingleName()
+    [TestCase("UserOne", true, new[] { "@userone" })]
+    [TestCase("UserOne", false, new[] { "userone" })]
+    [TestCase("UserOne,UserTwo,UserThree", true, new[] { "@userone", "@usertwo", "@userthree" })]
+    [TestCase("UserOne,UserTwo,UserThree", false, new[] { "userone", "usertwo", "userthree" })]
+    [TestCase("UserOne; UserTwo UserThree", true, new[] { "@userone", "@usertwo", "@userthree" })]
+    [TestCase("UserOne; UserTwo UserThree", false, new[] { "userone", "usertwo", "userthree" })]
+    [TestCase(" UserOne ,  UserTwo;UserThree ", true, new[] { "@userone", "@usertwo", "@userthree" })]
+    [TestCase(" UserOne ,  UserTwo;UserThree ", false, new[] { "userone", "usertwo", "userthree" })]
+    [TestCase("", true, new string[0])]
+    [TestCase("", false, new string[0])]
+    public void NormilizeTgNames_ReturnsExpected(string input, bool isAddAt, string[] expected)
     {
-        var result = TgStringUtils.NormilizeTgNames("UserOne");
+        var result = TgStringUtils.NormilizeTgNames(input, isAddAt);
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void NormilizeTgNames_SingleName_AddAt()
+    {
+        var result = TgStringUtils.NormilizeTgNames("UserOne", isAddAt: true);
+        Assert.That(result, Is.EqualTo(new List<string> { "@userone" }));
+    }
+
+    [Test]
+    public void NormilizeTgNames_SingleName_NoAddAt()
+    {
+        var result = TgStringUtils.NormilizeTgNames("UserOne", isAddAt: false);
         Assert.That(result, Is.EqualTo(new List<string> { "userone" }));
     }
 
     [Test]
-    public void NormilizeTgNames_CommaSeparated()
+    public void NormilizeTgNames_CommaSeparated_AddAt()
     {
-        var result = TgStringUtils.NormilizeTgNames("UserOne,UserTwo,UserThree");
+        var result = TgStringUtils.NormilizeTgNames("UserOne,UserTwo,UserThree", isAddAt: true);
+        Assert.That(result, Is.EqualTo(new List<string> { "@userone", "@usertwo", "@userthree" }));
+    }
+
+    [Test]
+    public void NormilizeTgNames_CommaSeparated_NoAddAt()
+    {
+        var result = TgStringUtils.NormilizeTgNames("UserOne,UserTwo,UserThree", isAddAt: false);
         Assert.That(result, Is.EqualTo(new List<string> { "userone", "usertwo", "userthree" }));
     }
 
     [Test]
-    public void NormilizeTgNames_SemicolonAndSpaceSeparated()
+    public void NormilizeTgNames_SemicolonAndSpaceSeparated_AddAt()
     {
-        var result = TgStringUtils.NormilizeTgNames("UserOne; UserTwo UserThree");
+        var result = TgStringUtils.NormilizeTgNames("UserOne; UserTwo UserThree", isAddAt: true);
+        Assert.That(result, Is.EqualTo(new List<string> { "@userone", "@usertwo", "@userthree" }));
+    }
+
+    [Test]
+    public void NormilizeTgNames_SemicolonAndSpaceSeparated_NoAddAt()
+    {
+        var result = TgStringUtils.NormilizeTgNames("UserOne; UserTwo UserThree", isAddAt: false);
         Assert.That(result, Is.EqualTo(new List<string> { "userone", "usertwo", "userthree" }));
     }
 
     [Test]
-    public void NormilizeTgNames_MixedSeparatorsAndSpaces()
+    public void NormilizeTgNames_MixedSeparatorsAndSpaces_AddAt()
     {
-        var result = TgStringUtils.NormilizeTgNames(" UserOne ,  UserTwo;UserThree ");
+        var result = TgStringUtils.NormilizeTgNames(" UserOne ,  UserTwo;UserThree ", isAddAt: true);
+        Assert.That(result, Is.EqualTo(new List<string> { "@userone", "@usertwo", "@userthree" }));
+    }
+
+    [Test]
+    public void NormilizeTgNames_MixedSeparatorsAndSpaces_NoAddAt()
+    {
+        var result = TgStringUtils.NormilizeTgNames(" UserOne ,  UserTwo;UserThree ", isAddAt: false);
         Assert.That(result, Is.EqualTo(new List<string> { "userone", "usertwo", "userthree" }));
     }
 

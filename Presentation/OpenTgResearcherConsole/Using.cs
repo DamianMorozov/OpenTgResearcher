ac3b93b42ab56756bc4aa91604658daefa48f70b
Damian Viktorovich Morozov <damian@morozov33.ru>
2025-09-12 14:30:00 +0300

    Removed outdated comments in `OpenTgResearcherConsole`

--- Presentation/OpenTgResearcherConsole/Using.cs
+++ Presentation/OpenTgResearcherConsole/Using.cs
@@ -1,7 +1,4 @@
-﻿// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
-
-global using Autofac;
+﻿global using Autofac;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Logging;

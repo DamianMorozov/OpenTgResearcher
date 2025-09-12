8770a63aea492fe5fbee3100b53eea8dd23b1768
Damian Viktorovich Morozov <damian@morozov33.ru>
2025-09-12 13:32:48 +0300

    Removed outdated comments in `TgInfrastructure`

--- Core/TgInfrastructure/Dtos/TgLicenseDto.cs
+++ Core/TgInfrastructure/Dtos/TgLicenseDto.cs
@@ -1,7 +1,4 @@
-﻿// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
-
-namespace TgInfrastructure.Dtos;
+﻿namespace TgInfrastructure.Dtos;
 
 /// <summary> License DTO </summary>
 public sealed partial class TgLicenseDto : ObservableRecipient
